e705735d99032fa29224bf311acdeaec693edafc
zcmarcus <zcm.box@gmail.com>
2020-03-31 20:11:51 -0500

    Initial commit

---
+++ src/main/java/org/zcmarcus/VideoLabelDetection.java
@@ -0,0 +1,284 @@
+//Copyright 2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+//PDX-License-Identifier: MIT-0 (For details, see https://github.com/awsdocs/amazon-rekognition-developer-guide/blob/master/LICENSE-SAMPLECODE.)
+
+package org.zcmarcus;
+
+import com.amazonaws.auth.policy.*;
+import com.amazonaws.auth.policy.Statement.Effect;
+import com.amazonaws.auth.policy.actions.SQSActions;
+import com.amazonaws.services.rekognition.AmazonRekognition;
+import com.amazonaws.services.rekognition.AmazonRekognitionClientBuilder;
+import com.amazonaws.services.rekognition.model.*;
+import com.amazonaws.services.sns.AmazonSNS;
+import com.amazonaws.services.sns.AmazonSNSClientBuilder;
+import com.amazonaws.services.sns.model.CreateTopicRequest;
+import com.amazonaws.services.sns.model.CreateTopicResult;
+import com.amazonaws.services.sqs.AmazonSQS;
+import com.amazonaws.services.sqs.AmazonSQSClientBuilder;
+import com.amazonaws.services.sqs.model.CreateQueueRequest;
+import com.amazonaws.services.sqs.model.Message;
+import com.amazonaws.services.sqs.model.QueueAttributeName;
+import com.amazonaws.services.sqs.model.SetQueueAttributesRequest;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class VideoLabelDetection {
+
+    private final Logger logger = LogManager.getLogger(this.getClass());
+
+    private static String sqsQueueName=null;
+    private static String snsTopicName=null;
+    private static String snsTopicArn = null;
+    private static String roleArn= null;
+    private static String sqsQueueUrl = null;
+    private static String sqsQueueArn = null;
+    private static String startJobId = null;
+    private static String bucket = null;
+    private static String video = null;
+    private static AmazonSQS sqs=null;
+    private static AmazonSNS sns=null;
+    private static AmazonRekognition rek = null;
+
+    private static NotificationChannel channel= new NotificationChannel()
+            .withSNSTopicArn(snsTopicArn)
+            .withRoleArn(roleArn);
+
+
+    public static void main(String[] args) throws Exception {
+
+        video = args[0];
+        bucket = args[1];
+        roleArn = args[2];
+
+        sns = AmazonSNSClientBuilder.defaultClient();
+        sqs= AmazonSQSClientBuilder.defaultClient();
+        rek = AmazonRekognitionClientBuilder.defaultClient();
+
+        CreateTopicandQueue();
+
+        //=================================================
+
+        StartLabelDetection(bucket, video);
+
+        if (GetSQSMessageSuccess()==true)
+            GetLabelDetectionResults();
+
+        //=================================================
+
+
+        DeleteTopicandQueue();
+        System.out.println("Done!");
+
+    }
+
+
+    static boolean GetSQSMessageSuccess() throws Exception
+    {
+        boolean success=false;
+
+
+        System.out.println("Waiting for job: " + startJobId);
+        //Poll queue for messages
+        List<Message> messages=null;
+        int dotLine=0;
+        boolean jobFound=false;
+
+        //loop until the job status is published. Ignore other messages in queue.
+        do{
+            messages = sqs.receiveMessage(sqsQueueUrl).getMessages();
+            if (dotLine++<40){
+                System.out.print(".");
+            }else{
+                System.out.println();
+                dotLine=0;
+            }
+
+            if (!messages.isEmpty()) {
+                //Loop through messages received.
+                for (Message message: messages) {
+                    String notification = message.getBody();
+
+                    // Get status and job id from notification.
+                    ObjectMapper mapper = new ObjectMapper();
+                    JsonNode jsonMessageTree = mapper.readTree(notification);
+                    JsonNode messageBodyText = jsonMessageTree.get("Message");
+                    ObjectMapper operationResultMapper = new ObjectMapper();
+                    JsonNode jsonResultTree = operationResultMapper.readTree(messageBodyText.textValue());
+                    JsonNode operationJobId = jsonResultTree.get("JobId");
+                    JsonNode operationStatus = jsonResultTree.get("Status");
+                    System.out.println("Job found was " + operationJobId);
+                    // Found job. Get the results and display.
+                    if(operationJobId.asText().equals(startJobId)){
+                        jobFound=true;
+                        System.out.println("Job id: " + operationJobId );
+                        System.out.println("Status : " + operationStatus.toString());
+                        if (operationStatus.asText().equals("SUCCEEDED")){
+                            success=true;
+                        }
+                        else{
+                            System.out.println("Video analysis failed");
+                        }
+
+                        sqs.deleteMessage(sqsQueueUrl,message.getReceiptHandle());
+                    }
+
+                    else{
+                        System.out.println("Job received was not job " +  startJobId);
+                        //Delete unknown message. Consider moving message to dead letter queue
+                        sqs.deleteMessage(sqsQueueUrl,message.getReceiptHandle());
+                    }
+                }
+            }
+            else {
+                Thread.sleep(5000);
+            }
+        } while (!jobFound);
+
+        System.out.println("Finished processing video");
+        return success;
+    }
+
+
+    private static void StartLabelDetection(String bucket, String video) throws Exception{
+
+        NotificationChannel channel= new NotificationChannel()
+                .withSNSTopicArn(snsTopicArn)
+                .withRoleArn(roleArn);
+
+
+        StartLabelDetectionRequest req = new StartLabelDetectionRequest()
+                .withVideo(new Video()
+                        .withS3Object(new S3Object()
+                                .withBucket(bucket)
+                                .withName(video)))
+                .withMinConfidence(50F)
+                .withJobTag("DetectingLabels")
+                .withNotificationChannel(channel);
+
+        StartLabelDetectionResult startLabelDetectionResult = rek.startLabelDetection(req);
+        startJobId=startLabelDetectionResult.getJobId();
+
+    }
+
+    private static void GetLabelDetectionResults() throws Exception{
+
+        int maxResults=10;
+        String paginationToken=null;
+        GetLabelDetectionResult labelDetectionResult=null;
+
+        do {
+            if (labelDetectionResult !=null){
+                paginationToken = labelDetectionResult.getNextToken();
+            }
+
+            GetLabelDetectionRequest labelDetectionRequest= new GetLabelDetectionRequest()
+                    .withJobId(startJobId)
+                    .withSortBy(LabelDetectionSortBy.TIMESTAMP)
+                    .withMaxResults(maxResults)
+                    .withNextToken(paginationToken);
+
+
+            labelDetectionResult = rek.getLabelDetection(labelDetectionRequest);
+
+            VideoMetadata videoMetaData=labelDetectionResult.getVideoMetadata();
+
+            System.out.println("Format: " + videoMetaData.getFormat());
+            System.out.println("Codec: " + videoMetaData.getCodec());
+            System.out.println("Duration: " + videoMetaData.getDurationMillis());
+            System.out.println("FrameRate: " + videoMetaData.getFrameRate());
+
+
+            //Show labels, confidence and detection times
+            List<LabelDetection> detectedLabels= labelDetectionResult.getLabels();
+
+            for (LabelDetection detectedLabel: detectedLabels) {
+                long seconds=detectedLabel.getTimestamp();
+                Label label=detectedLabel.getLabel();
+                System.out.println("Millisecond: " + Long.toString(seconds) + " ");
+
+                System.out.println("   Label:" + label.getName());
+                System.out.println("   Confidence:" + detectedLabel.getLabel().getConfidence().toString());
+
+                List<Instance> instances = label.getInstances();
+                System.out.println("   Instances of " + label.getName());
+                if (instances.isEmpty()) {
+                    System.out.println("        " + "None");
+                } else {
+                    for (Instance instance : instances) {
+                        System.out.println("        Confidence: " + instance.getConfidence().toString());
+                        System.out.println("        Bounding box: " + instance.getBoundingBox().toString());
+                    }
+                }
+                System.out.println("   Parent labels for " + label.getName() + ":");
+                List<Parent> parents = label.getParents();
+                if (parents.isEmpty()) {
+                    System.out.println("        None");
+                } else {
+                    for (Parent parent : parents) {
+                        System.out.println("        " + parent.getName());
+                    }
+                }
+                System.out.println();
+            }
+        } while (labelDetectionResult !=null && labelDetectionResult.getNextToken() != null);
+
+    }
+
+    // Creates an SNS topic and SQS queue. The queue is subscribed to the topic. 
+    static void CreateTopicandQueue()
+    {
+        //create a new SNS topic
+        snsTopicName="AmazonRekognitionTopic" + Long.toString(System.currentTimeMillis());
+        CreateTopicRequest createTopicRequest = new CreateTopicRequest(snsTopicName);
+        CreateTopicResult createTopicResult = sns.createTopic(createTopicRequest);
+        snsTopicArn=createTopicResult.getTopicArn();
+
+        //Create a new SQS Queue
+        sqsQueueName="AmazonRekognitionQueue" + Long.toString(System.currentTimeMillis());
+        final CreateQueueRequest createQueueRequest = new CreateQueueRequest(sqsQueueName);
+        sqsQueueUrl = sqs.createQueue(createQueueRequest).getQueueUrl();
+        sqsQueueArn = sqs.getQueueAttributes(sqsQueueUrl, Arrays.asList("QueueArn")).getAttributes().get("QueueArn");
+
+        //Subscribe SQS queue to SNS topic
+        String sqsSubscriptionArn = sns.subscribe(snsTopicArn, "sqs", sqsQueueArn).getSubscriptionArn();
+
+        // Authorize queue
+        Policy policy = new Policy().withStatements(
+                new Statement(Effect.Allow)
+                        .withPrincipals(Principal.AllUsers)
+                        .withActions(SQSActions.SendMessage)
+                        .withResources(new Resource(sqsQueueArn))
+                        .withConditions(new Condition().withType("ArnEquals").withConditionKey("aws:SourceArn").withValues(snsTopicArn))
+        );
+
+
+        Map queueAttributes = new HashMap();
+        queueAttributes.put(QueueAttributeName.Policy.toString(), policy.toJson());
+        sqs.setQueueAttributes(new SetQueueAttributesRequest(sqsQueueUrl, queueAttributes));
+
+
+        System.out.println("Topic arn: " + snsTopicArn);
+        System.out.println("Queue arn: " + sqsQueueArn);
+        System.out.println("Queue url: " + sqsQueueUrl);
+        System.out.println("Queue sub arn: " + sqsSubscriptionArn );
+    }
+    static void DeleteTopicandQueue()
+    {
+        if (sqs !=null) {
+            sqs.deleteQueue(sqsQueueUrl);
+            System.out.println("SQS queue deleted");
+        }
+
+        if (sns!=null) {
+            sns.deleteTopic(snsTopicArn);
+            System.out.println("SNS topic deleted");
+        }
+    }
+}
---
+++ src/test/java/org/zcmarcus/VideoLabelDetectionTest.java
@@ -0,0 +1,20 @@
+package org.zcmarcus;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for VideoLableDetectionTest
+ */
+public class VideoLabelDetectionTest
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
